d7353b12484632299c51388ce8f1fd3b363efb0c
federicogonzalez1988 <federico.gon88@gmail.com>
2026-09-11 22:04:55 -0300

    Clase Pedido modificada con la nueva Jerarquia de la clase Comida

--- src/pedido.ts
+++ src/pedido.ts
@@ -1,26 +1,16 @@
 export default class Pedido {
-    private productosIndividuales: Item[];
-    private productosCombinados: Combo[];
+    private productos: Comida[];
     private numeroPedido: string;
     //private estadoPedido:     falta desarrollar
 
-    //en esta primera instancia creamos el constructor como si siempre recibiera elementos de tipo Item y de tipo Combo, no estamos evaluando la posibilidad de que reciba solo Items o solo Combos. Con sobrecarga "no se podría" hacer ya que TypeScript no reconoce los tipos de datos que no sean nativos, recibiria un Array y no sabria si es de tipo Item o de tipo Combo. Otra posibilidad: que todos los Pedidos se inicien vacios y que se vayan agregando Items y Combos.
-    public constructor(numeroPedido:string, productosIndividuales:Item[], productosCombinados:Combo[]) {
+    public constructor(numeroPedido:string, productos:Comida[] = []) {
         this.numeroPedido=numeroPedido;
-        this.productosIndividuales=productosIndividuales;
-        this.productosCombinados=productosCombinados;
+        this.productos=productos;
     }
 
-    public agregarItem(item:Item) {
-        //compruebo si el item está disponible, habria que ver el nombre de ese atributo, podria ser un boolean 'estaDisponible'
-        if(item.estado) {
-            this.productosIndividuales.push(item);
-        }
-    }
-
-    public agregarCombo(combo:Combo) {
-        if(combo.estaDisponible) {
-            this.productosCombinados.push(combo);
-        }
+    public agregarProducto(producto:Comida) {
+        if(producto.disponible) {
+            this.productos.push(producto);
+        } //falta excepcion por producto no disponible
     }
 }
